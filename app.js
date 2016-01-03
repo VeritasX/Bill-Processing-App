c6cc134353e470cbaaf399b0d71824e649511af6
veritasx <aaronendsley@gmail.com>
2016-01-02 19:19:43 -0600

    fixes

---
+++ app.js
@@ -0,0 +1,3 @@
+'use strict'
+
+var tAmount=document.getElementById('final_total'), bill = document.getElementById('bill'), amount = document.getElementById('amount'), submit = document.getElementById('submit'), output = document.getElementById('output');
