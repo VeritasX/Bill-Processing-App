30564be6bf8df961d11700abad4b9a1de395550e
veritasx <aaronendsley@gmail.com>
2016-01-02 19:59:03 -0600

    planning

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 'use strict'
 
-var tAmount=document.getElementById('final_total'), bill = document.getElementById('bill'), amount = document.getElementById('amount'), submit = document.getElementById('submit'), output = document.getElementById('output');
+var tAmount=document.getElementById('final_total'), bill = document.getElementById('bill'), amount = document.getElementById('amount'), submit = document.getElementById('submit'), output = document.getElementById('output'), outputAmount, bills=[];
+
+
+//allow the user to input a bill
+//When the user clicks the submit button bill is put into an array.
+
+
+
+//allow the user to input a amount
+//when the user clicks the submit button it adds the amouunt of the bill to an array and then adds the amount of the total in the array and stores it in an outputAmount variable
