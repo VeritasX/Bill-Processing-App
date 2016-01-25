6d0c05f2caa366ee1f2752784b7febc41e174c9e
veritasx <aaronendsley@gmail.com>
2016-01-24 18:39:21 -0600

    Finished implementing functionality

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
 'use strict';
-var BillsOutPut = [],
-  AmountsToAdd = [];
+var BillsOutPut = [];
+var AmountsToAdd = [];
+var costAmount = 0;
+var parseCost;
+var AmountChanged;
+
+
 var submitThis = function() {
-  var bill = document.getElementById('bill').value,
-    amount = document.getElementById('amount').value;
+  var bill = document.getElementById('bill').value;
+  var amount = document.getElementById('amount').value;
+  var outputDiv = document.getElementById('output');
+
+  function parseAmount() {
+    parseCost = parseFloat(amount).toFixed(2);
+  }
+
+  function modifyAmount() {
+    AmountChanged = '$' + parseCost;
+  }
+  function addTheCost() {
+    var parseIt = parseFloat(amount);
+    costAmount = costAmount + parseIt;
+  }
+
+  while (outputDiv.hasChildNodes()) {
+    outputDiv.removeChild(outputDiv.firstChild);
+  }
+  parseAmount();
+  modifyAmount();
+  addTheCost();
   BillsOutPut.push(bill);
-  AmountsToAdd.push(amount);
+  AmountsToAdd.push(AmountChanged);
+  var totalAmount = costAmount.toFixed(2);
+  for (let i = 0; i < BillsOutPut.length; i++) {
+    outputDiv.innerHTML += '<p> Bill: ' + BillsOutPut[i] + '&#8195;&#8195;&#8195;&#8195; Amount: &#8195;' + AmountsToAdd[i] + '    </p>';
+  }
+  outputDiv.innerHTML += '<h3> Final Total: $' + totalAmount + ' </h3>';
+
+
 }
 
 
