20a6a5aeb29633acb04b4dd368ab3c8c172cbe1f
veritasx <aaronendsley@gmail.com>
2016-01-24 19:12:08 -0600

    feedback initialized

--- app.js
+++ app.js
@@ -7,37 +7,45 @@ var AmountChanged;
 
 
 var submitThis = function() {
+
   var bill = document.getElementById('bill').value;
   var amount = document.getElementById('amount').value;
   var outputDiv = document.getElementById('output');
-
-  function parseAmount() {
-    parseCost = parseFloat(amount).toFixed(2);
-  }
-
-  function modifyAmount() {
-    AmountChanged = '$' + parseCost;
-  }
-  function addTheCost() {
-    var parseIt = parseFloat(amount);
-    costAmount = costAmount + parseIt;
-  }
-
-  while (outputDiv.hasChildNodes()) {
-    outputDiv.removeChild(outputDiv.firstChild);
-  }
-  parseAmount();
-  modifyAmount();
-  addTheCost();
-  BillsOutPut.push(bill);
-  AmountsToAdd.push(AmountChanged);
-  var totalAmount = costAmount.toFixed(2);
-  for (let i = 0; i < BillsOutPut.length; i++) {
-    outputDiv.innerHTML += '<p> Bill: ' + BillsOutPut[i] + '&#8195;&#8195;&#8195;&#8195; Amount: &#8195;' + AmountsToAdd[i] + '    </p>';
-  }
-  outputDiv.innerHTML += '<h3> Final Total: $' + totalAmount + ' </h3>';
+  function getYourBills() {
 
 
+    function parseAmount() {
+      parseCost = parseFloat(amount).toFixed(2);
+    }
+
+    function modifyAmount() {
+      AmountChanged = '$' + parseCost;
+    }
+    function addTheCost() {
+      var parseIt = parseFloat(amount);
+      costAmount = costAmount + parseIt;
+    }
+
+    while (outputDiv.hasChildNodes()) {
+      outputDiv.removeChild(outputDiv.firstChild);
+    }
+    parseAmount();
+    modifyAmount();
+    addTheCost();
+    BillsOutPut.push(bill);
+    AmountsToAdd.push(AmountChanged);
+    var totalAmount = costAmount.toFixed(2);
+    for (let i = 0; i < BillsOutPut.length; i++) {
+      outputDiv.innerHTML += '<p> Bill: ' + BillsOutPut[i] + '&#8195;&#8195;&#8195;&#8195; Amount: &#8195;' + AmountsToAdd[i] + '    </p>';
+    }
+    outputDiv.innerHTML += '<h3> Final Total: $' + totalAmount + ' </h3>';
+  }
+
+  if (amount == null) {
+    outputDiv.innerHtml += '<h3> Please input your bills</h3>';
+  } else {
+    getYourBills();
+  }
 }
 
 
